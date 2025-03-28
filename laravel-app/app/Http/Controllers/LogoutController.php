18107390d5eca6367730e23b5a9320d255f24895
nhango2023 <ngothanhnha2017@gmail.com>
2025-03-28 23:34:29 +0700

    hoan thanh them, sua, xoa user (admin)

---
+++ laravel-app/app/Http/Controllers/LogoutController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class LogoutController extends Controller
+{
+    public function index(Request $request)
+    {
+
+        Auth::logout(); // Đăng xuất user
+
+        $request->session()->invalidate(); // Xóa session cũ
+        $request->session()->regenerateToken(); // Tạo CSRF token mới
+
+        return redirect('/')->with('success', 'Đăng xuất thành công!');
+    }
+}
